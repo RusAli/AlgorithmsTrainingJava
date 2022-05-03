73b5212084c31b95f86597ac1e9c6407f8185c6d
RusAli <mr.al1mov.ruslan@gmail.com>
2022-05-04 00:51:59 +0300

    created NumberOccurrences method with tests

--- src/main/java/homework/HW9/NumberOccurrences.java
+++ src/main/java/homework/HW9/NumberOccurrences.java
@@ -1,37 +1,46 @@
 package homework.HW9;
 
+import java.util.Arrays;
+
 public class NumberOccurrences {
 
   public int[][] numberOccurrences(int[] arr){
     if(arr != null && arr.length>0){
 
+      Arrays.sort(arr);
+      int number = 0;
       for (int i = 0; i < arr.length; i++) {
-        for (int j = i+1; j < arr.length; j++) {
-
+        for (int j = i + 1; j < arr.length; j++) {
+          if (arr[j] == arr[i]) {
+            break;
+          } else {
+            number++;
+            break;
+          }
         }
       }
 
+      int[][] result = new int[number + 1][2];
 
+      number = 0;
+      int count = 0;
 
+      for (int i = 0; i < arr.length; i += count) {
+        result[number][0] = arr[i];
+        count = 0;
+        for (int j = 0; j < arr.length; j++) {
+          if (arr[j] == arr[i]) {
+            count++;
+          }
+        }
+        result[number][1] = count;
+        number++;
+      }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      return result;
     }
 
     return new int[][]{};
   }
-}
+ }
+
--- src/test/java/HW9/NumberOccurrencesTest.java
+++ src/test/java/HW9/NumberOccurrencesTest.java
@@ -1,4 +1,35 @@
 package HW9;
 
+import homework.HW9.NumberOccurrences;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class NumberOccurrencesTest {
+
+  @Test
+  public void testNumberOccurrences(){
+    int[] arr = {3, 2, 5, 3, 1, 5, 4, 2, 1, 4, 5, 3, 2, 1, 4, 5, 3, 2, 1};
+    int[][] expected = {{1, 4}, {2, 4}, {3, 4}, {4, 3}, {5, 4}};
+
+    NumberOccurrences numberOccurrences = new NumberOccurrences();
+    int[][] actual = numberOccurrences.numberOccurrences(arr);
+
+    Assertions.assertTrue(Arrays.deepEquals(expected,actual));
+  }
+
+  @Test
+  public void testNumberOccurrencesWithEmptyArr(){
+    int[] arr = {};
+    int[][] expected = {};
+
+    NumberOccurrences numberOccurrences = new NumberOccurrences();
+    int[][] actual = numberOccurrences.numberOccurrences(arr);
+
+    Assertions.assertTrue(Arrays.deepEquals(expected,actual));
+  }
+
+
+
 }
