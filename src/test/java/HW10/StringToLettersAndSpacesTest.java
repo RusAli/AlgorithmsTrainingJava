6fb143eb4d54634a45bc3dad5f931344996e5671
RusAli <mr.al1mov.ruslan@gmail.com>
2022-05-16 22:33:20 +0300

    created StringToLettersAndSpaces method with test

---
+++ src/test/java/HW10/StringToLettersAndSpacesTest.java
@@ -0,0 +1,21 @@
+package HW10;
+
+import homework.HW10.StringToLetters;
+import homework.HW10.StringToLettersAndSpaces;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringToLettersAndSpacesTest {
+
+  @Test
+  public void testStringToLettersAndSpacesTest(){
+    String str = "q@34 w?><e%r(*t0 013y";
+    String expected = "q wert y";
+
+    StringToLettersAndSpaces stringToLettersAndSpaces = new StringToLettersAndSpaces();
+    String actual = stringToLettersAndSpaces.stringToLettersAndSpaces(str);
+
+    Assertions.assertEquals(expected,actual);
+  }
+
+}
